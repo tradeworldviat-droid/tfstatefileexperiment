55ddc9e1d21265eebba1007e083c2d76ff676c57
Swapnil <swapnilpawarsup@gmail.com>
2026-04-01 22:35:24 +0530

    revise backup file

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "mybucket-backend.tf"
-    key            = "tfstatefolder"
-    region         = "ap-south-1"
+    bucket = "mybucket-backend-tf"
+    key    = "terraform/state/terraform.tfstate"
+    region = "ap-south-1"
   }
 }
