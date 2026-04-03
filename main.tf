4907b3c97361cc85851f469b7ce54f3ee377d784
root <root@ip-172-31-1-128.ap-south-1.compute.internal>
2026-04-03 01:23:58 +0000

    trying tags modifiendg test

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "name" {
 	instance_type = "t3.micro"
 
   tags = {
-    Name = "change from remote location2"
+    Name = "change from remote location-onemore time tryed"
   }
 }
