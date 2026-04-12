488ec33668db945e6d118a054d99467ebaa517b7
Swapnil <swapnilpawarsup@gmail.com>
2026-04-12 12:18:40 +0530

    name tag modifired for new jenkinserver pipline

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ resource "aws_instance" "name" {
 	instance_type = "t3.micro"
 
   tags = {
-    Name = "change from remote location-onemore time tryed"
+    Name = "change from remote location-onemore time tryed-(actually second)"
   }
 }
