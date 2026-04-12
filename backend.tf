98b204c3f46c67ddc0d88e668ae827ecdd1d55cf
Swapnil <swapnilpawarsup@gmail.com>
2026-04-12 17:14:57 +0530

    now implimented statelocking trick

--- backend.tf
+++ backend.tf
@@ -3,5 +3,6 @@ terraform {
     bucket = "mybucket-backend.tf"
     key    = "terraform/state/terraform.tfstate"
     region = "ap-south-1"
+    use_lockfile = true         #only supprove verstion above 1.10
   }
 }
